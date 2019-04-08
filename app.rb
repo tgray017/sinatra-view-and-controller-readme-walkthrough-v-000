19a297b133b77c591042195e934f298f888a39e5
Thomas Gray <tgray017@gmail.com>
2019-04-08 11:08:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,8 +6,8 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
-    reversed_string = params[:string].reverse
-    binding.pry
+    @reversed_string = params[:string].reverse
+
     erb :reversed
   end
 
